723103f3cebff5ee49c72b5626788069f9f1dfe1
endqsx <endqsxmarquis@gmail.com>
2026-08-11 06:08:17 -0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
+        name: "Back to School.", // required by Discord API, not shown in the client
         state: "valorant",     // this is what people actually see
         type: 0,               // Custom
       },
